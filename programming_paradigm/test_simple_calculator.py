0b6ec47faf87de06980e7ffe2a57486c7520f533
rutokenneth <rutokenneth16@gmail.com>
2025-06-16 14:10:48 +0300

    it2

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,50 +4,36 @@ from simple_calculator import SimpleCalculator
 class TestSimpleCalculator(unittest.TestCase):
 
     def setUp(self):
-        """Set up a calculator instance for each test."""
+        """Set up the SimpleCalculator instance before each test."""
         self.calc = SimpleCalculator()
 
-    def test_add_positive_numbers(self):
-        self.assertEqual(self.calc.add(10, 5), 15)
-
-    def test_add_negative_numbers(self):
+    def test_addition(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(-2, -3), -5)
+        self.assertEqual(self.calc.add(0, 0), 0)
 
-    def test_add_zero(self):
-        self.assertEqual(self.calc.add(0, 5), 5)
-
-    def test_subtract_positive_numbers(self):
+    def test_subtraction(self):
+        """Test the subtraction method."""
         self.assertEqual(self.calc.subtract(10, 5), 5)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
+        self.assertEqual(self.calc.subtract(-3, -2), -1)
+        self.assertEqual(self.calc.subtract(5, 10), -5)
 
-    def test_subtract_negative_numbers(self):
-        self.assertEqual(self.calc.subtract(-5, -3), -2)
+    def test_multiplication(self):
+        """Test the multiplication method."""
+        self.assertEqual(self.calc.multiply(4, 5), 20)
+        self.assertEqual(self.calc.multiply(-2, 3), -6)
+        self.assertEqual(self.calc.multiply(-2, -3), 6)
+        self.assertEqual(self.calc.multiply(0, 10), 0)
 
-    def test_subtract_result_negative(self):
-        self.assertEqual(self.calc.subtract(3, 5), -2)
-
-    def test_multiply_positive_numbers(self):
-        self.assertEqual(self.calc.multiply(3, 4), 12)
-
-    def test_multiply_with_zero(self):
-        self.assertEqual(self.calc.multiply(5, 0), 0)
-
-    def test_multiply_negative_numbers(self):
-        self.assertEqual(self.calc.multiply(-3, -4), 12)
-
-    def test_multiply_positive_and_negative(self):
-        self.assertEqual(self.calc.multiply(3, -4), -12)
-
-    def test_divide_normal(self):
+    def test_division(self):
+        """Test the division method."""
         self.assertEqual(self.calc.divide(10, 2), 5)
-
-    def test_divide_by_zero(self):
-        self.assertIsNone(self.calc.divide(5, 0))
-
-    def test_divide_negative_numbers(self):
-        self.assertEqual(self.calc.divide(-8, -2), 4)
-
-    def test_divide_positive_and_negative(self):
-        self.assertEqual(self.calc.divide(9, -3), -3)
+        self.assertEqual(self.calc.divide(-10, 2), -5)
+        self.assertEqual(self.calc.divide(0, 1), 0)
+        self.assertIsNone(self.calc.divide(5, 0))  # Division by zero
 
 if __name__ == '__main__':
     unittest.main()
