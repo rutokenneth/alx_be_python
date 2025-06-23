f84a382dfdaccb0cb31beb5c9894f34c7f9170e9
rutokenneth <rutokenneth16@gmail.com>
2025-06-23 10:43:10 +0300

    book class

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,1 +1,16 @@
+class Book:
+    def __init__(self, title, author, year):
+        """"""
+        self.title = title
+        self.author = author
+        self.year = year
+        print(f"Book '{self.title}' created.") #added for clarity during book instantiation
 
+    def __del__(self):
+        print(f"Deleting {self.title}")
+
+    def __str__(self):
+        return f"Book('{self.title}', {self.year})"
+    
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
